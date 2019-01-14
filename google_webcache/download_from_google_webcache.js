d38b9567de984ed65ae2320e1ae067a925173fe4
PincongBot <PincongBot@pm.me>
2019-01-14 16:37:26 +0800

    从 Google Webcache 获取备份

--- google_webcache/download_from_google_webcache.js
+++ google_webcache/download_from_google_webcache.js
@@ -8,6 +8,32 @@ const baseURL = "https://webcache.googleusercontent.com/search?vwsrc=1&q=cache:"
 const baseFilePath = "../../p/"
 const agent = getProxyAgent()
 
+const loadMetaData = (file) => {
+    const filePath = path.join(baseFilePath, file)
+
+    if (fs.existsSync(filePath)) {
+        return fs.readJSONSync(path.join(baseFilePath, file)) || []
+    } else {
+        return []
+    }
+}
+
+/**
+ * @param {string} file 
+ * @param {Iterable} metadata 
+ */
+const saveMetaData = (file, metadata) => {
+    return fs.writeJSON(
+        path.join(baseFilePath, file),
+        [...metadata].sort(),
+        { spaces: 4 }
+    )
+}
+
+const failed = new Set(loadMetaData("uploads_failed.json"))
+const successful = new Set(loadMetaData("uploads_successful.json"))
+
+
 /**
  * @typedef {{ postID: number; title: string; link: string; cacheID: string; }} Page
  */
@@ -22,6 +48,9 @@ const download = async (page) => {
     try {
         const r = await fetch(url, {
             timeout: 10000,
+            headers: {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) Gecko/20100101 Firefox/64.0",
+            },
             agent
         })
         if (r.ok) {
@@ -37,29 +66,53 @@ const download = async (page) => {
             const pageHTML = document.body.querySelector("div > pre").textContent
             await fs.writeFile(filePath, pageHTML)
 
+            failed.delete(url)
+            successful.add(url)
+
             return
         } else {
-            console.error(saveTo + " " + r.status)
+            console.error(url + " " + r.status + " " + r.statusText)
+            failed.add(url)
             return
         }
     } catch (e) {
+        failed.add(url)
         return console.error(saveTo + " " + e)
     }
 
 }
 
-const downloadAll = async () => {
-
-    /** @type {Page[]} */
-    const cachedPages = await fs.readJSON("./cached_pages.json")
+/**
+ * @param {Page[]} pages 
+ */
+const downloadAll = async (pages) => {
 
     await Promise.all(
-        cachedPages.map((page) => {
+        pages.map((page) => {
             return download(page)
         })
     )
 
+    saveMetaData("uploads_failed.json", failed)
+    saveMetaData("uploads_successful.json", successful)
+
+    console.log(successful.size + " 项下载成功")
+    console.log(failed.size + " 项下载失败")
+
+}
+
+const main = async (start = 0) => {
+
+    /** @type {Page[]} */
+    const cachedPages = await fs.readJSON("./cached_pages.json")
+
+    // 绕过网站的并发限制
+    const n = 2
+    for (let i = start; i <= cachedPages.length; i = i + n) {
+        await downloadAll(cachedPages.slice(i, i + n))
+    }
+
 }
 
 
-downloadAll()
+main()
