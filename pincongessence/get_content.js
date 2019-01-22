262af792dee22907289992f5e4292072fce07aa7
PincongBot <PincongBot@pm.me>
2019-01-22 18:32:48 +0800

    品葱搬运工内容备份的图片转成 Data URL 格式

--- pincongessence/get_content.js
+++ pincongessence/get_content.js
@@ -1,13 +1,14 @@
 // @ts-check
 
-const fs = require("fs")
+const fs = require("fs-extra")
 const path = require("path")
 const { JSDOM } = require("jsdom")
+const { fetch, getProxyAgent } = require("../util")
+
+const agent = getProxyAgent()
 
 const inputDir = "../../pincongessence/backup/"
 const outputDir = "../../pincongessence/content/"
-
-const files = fs.readdirSync(inputDir)
 
 const template = `
 <!DOCTYPE html>
@@ -23,21 +24,64 @@ const template = `
 /**
  * @param {string} fileName
  */
-const handler = (fileName) => {
+const handler = async (fileName) => {
 
     const inputFilePath = path.join(inputDir, fileName)
-    const html = fs.readFileSync(inputFilePath, "utf-8")
+    const html = await fs.readFile(inputFilePath, "utf-8")
 
     const { window: { document } } = new JSDOM(html)
 
     const articleElement = document.querySelector("article#_tl_editor")
-    const content = articleElement.innerHTML
 
+    const imgs = articleElement.querySelectorAll("img")
+
+    await Promise.all(
+        [...imgs].map(async (img) => {
+            const url = img.src
+
+            try {
+                const r = await fetch(url, {
+                    timeout: 10000,
+                    agent
+                })
+                if (r.ok) {
+                    const mime = r.headers.get("content-type")
+                    const data = await r.buffer()
+                    const base64 = data.toString("base64")
+                    const dataURL = `data:${mime};base64,${base64}`
+
+                    img.src = dataURL
+                } else {
+                    console.error(url + " " + r.status)
+                }
+            } catch (e) {
+                console.error(url + " " + e)
+            }
+        })
+    )
+
+    const content = articleElement.innerHTML
     const output = template.replace("{content}", content)
 
     const outputFilePath = path.join(outputDir, fileName)
-    fs.writeFileSync(outputFilePath, output)
+    await fs.writeFile(outputFilePath, output)
 
 }
 
-files.forEach(handler)
+const main = async (start = 0) => {
+
+    const files = fs.readdirSync(inputDir)
+
+    const n = 10
+    for (let i = start; i <= files.length; i = i + n) {
+
+        await Promise.all(
+            files.slice(i, i + n).map((fileName) => {
+                return handler(fileName)
+            })
+        )
+
+    }
+}
+
+main()
