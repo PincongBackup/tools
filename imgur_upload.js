af700b6b731f346d9411e8849d3d450ffb009b72
PincongBot <PincongBot@pm.me>
2019-01-22 21:28:59 +0800

    品葱搬运工的内容备份转换成  bbcode 格式

---
+++ imgur_upload.js
@@ -0,0 +1,38 @@
+// @ts-check
+const { URLSearchParams } = require("url")
+const { fetch, getProxyAgent } = require("./util")
+
+const agent = getProxyAgent()
+
+/**
+ * 上传到 imgur.com
+ * @param {string} image 图片地址或图片的base64数据
+ * @return {Promise<string>} 图片在 imgur.com 上的地址
+ */
+const upload = async (image) => {
+
+    const params = new URLSearchParams()
+    params.append("image", image)
+
+    const r = await fetch("https://api.imgur.com/3/image", {
+        method: "POST",
+        body: params,
+        headers: {
+            "Authorization": "Client-ID f0ea04148a54268",
+        },
+        timeout: 10000,
+        agent,
+    })
+
+    const json = await r.json()
+
+    if (!json.success) {
+        console.error("status:", json.status)
+        throw new Error(json.data.error)
+    }
+
+    return json.data.link
+
+}
+
+module.exports = upload
---
+++ pincongessence/to_bbcode.js
@@ -0,0 +1,113 @@
+// @ts-check
+const fs = require("fs-extra")
+const path = require("path")
+const { JSDOM } = require("jsdom")
+const { HTML2BBCode } = require("html2bbcode")
+const upload = require("../imgur_upload")
+
+const officialListFilePath = "../../pincongessence/official_list.json"
+const listFilePath = "../../pincongessence/list.json"
+
+const inputDir = "../../pincongessence/backup/"
+const outputDir = "../../pincongessence/bbcode/"
+
+const bbcodeConverter = new HTML2BBCode({
+    noalign: true,
+    noheadings: true,
+})
+
+/**
+ * @param {string} fileName
+ * @param {number} index
+ */
+const handler = async (fileName, index) => {
+
+    const inputFilePath = path.join(inputDir, fileName)
+    const html = await fs.readFile(inputFilePath, "utf-8")
+
+    const { window: { document } } = new JSDOM(html)
+
+    const articleElement = document.querySelector("article#_tl_editor")
+
+    const titleElement = articleElement.children[0]
+    titleElement.outerHTML = `<b>${titleElement.textContent.trim()}</b><br>`
+
+    const addressElement = document.querySelector("article#_tl_editor > address")
+    addressElement.innerHTML = "<br><br>[i]@" + addressElement.textContent.trim() + "：[/i]<br><br>"
+
+    const imgs = articleElement.querySelectorAll("img")
+    await Promise.all(
+        [...imgs].map(async (img) => {
+            const url = img.src
+
+            try {
+                img.src = await upload(url)
+            } catch (e) {
+                console.error(fileName + " " + url + " " + e)
+            }
+        })
+    )
+
+    const H4List = articleElement.querySelectorAll("h4")
+    if (H4List.length > 0) {
+        const lastH4 = H4List[H4List.length - 1]
+        lastH4.after(addressElement)
+
+        H4List.forEach((h4) => {
+            h4.outerHTML = `<br><b class="h4">${h4.innerHTML}</b><br>`
+        })
+    }
+
+    const title = document.querySelector(".tl_article_header > h1").textContent.trim()
+    const quote = `\n[quote]${index + 1}. ${title}[/quote]\n\n`
+
+    const content = articleElement.innerHTML
+    const bbcode = bbcodeConverter.feed(content)
+    const output = quote + bbcode.toString()
+
+    const outputFilePath = path.join(outputDir, `${index + 1}.bbcode`)
+    fs.ensureDirSync(path.parse(outputFilePath).dir)
+    await fs.writeFile(outputFilePath, output)
+
+}
+
+const main = async (start = 0) => {
+
+    /** @type {{ link: string; }[]} */
+    const officialList = (await fs.readJSON(officialListFilePath)).slice(0, 737)
+
+    /** @type {{ link: string; messageLink: string; }[]} */
+    const list = await fs.readJSON(listFilePath)
+
+    const articlesMap = new Map(
+        list.map((x) => {
+            const { link, messageLink } = x
+            const fileName = link.split("/").pop() + ".html"
+
+            /** @type {[string, string]} */
+            const article = [messageLink, fileName]
+
+            return article
+        })
+    )
+
+    const l = officialList.map((x, index) => {
+        return {
+            fileName: articlesMap.get(x.link),
+            index,
+        }
+    })
+
+    const n = 10
+    for (let i = start; i <= l.length; i = i + n) {
+
+        await Promise.all(
+            l.slice(i, i + n).map(({ fileName, index }) => {
+                return handler(fileName, index)
+            })
+        )
+
+    }
+}
+
+main()
